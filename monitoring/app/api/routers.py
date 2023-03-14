a75e85a1fb91029d93a2d7090972eb7f38e6a130
Fernando Bertoncello Bellelis <104454364+nandobb1411@users.noreply.github.com>
2023-03-13 22:16:36 -0300

    Delete monitoring directory

--- monitoring/app/api/routers.py
+++
@@ -1,9 +0,0 @@
-"""This module includes API routers."""
-from fastapi import APIRouter
-from .endpoints.performance import router as perf_router
-from .endpoints.aderencia import router as ader_router
-router = APIRouter()
-
-# Routers from each endpoint
-router.include_router(perf_router)
-router.include_router(ader_router)
